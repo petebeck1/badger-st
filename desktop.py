261ea8c51410220c7ffde1d1b433bd16de7035b8
Pete Beck <pete@kstc.org.uk>
2023-03-22 14:54:55 +0000

    Added icons

--- desktop.py
+++ desktop.py
@@ -12,12 +12,7 @@ def menu(display):
     display.pen(0)
     display.line(0, 17, badger2040.WIDTH, 17)
     write(display, ' Desk  File  View  Options ', 0, 0)
-    
-    
-def desktop(display):
-    background(display)
-    menu(display)
-    
+
 
 def window(display, s, x, y, w, h):
     display.pen(15)
@@ -26,7 +21,7 @@ def window(display, s, x, y, w, h):
     display.thickness(1)
 
     # Header
-    char(display, 'close', x + 4, y + 1)
+    char(display, 'close', x + 5, y + 1)
     char(display, 'full', x + w - 11, y + 1)
     display.pen(12)
     display.rectangle(x + 17, y + 1, w - 34, 16)
@@ -41,7 +36,7 @@ def window(display, s, x, y, w, h):
     
     # Scroll bars
     char(display, 'resize', x + w - 11, y + h - 15)
-    char(display, 'left', x + 4, y + h - 15)
+    char(display, 'left', x + 5, y + h - 15)
     char(display, 'right', x + w - 27, y + h - 15)
     char(display, 'up', x + w - 11, y + 18)
     char(display, 'down', x + w - 11, y + h - 31)
@@ -76,3 +71,45 @@ def msgbox(display, s, x, y, w, h):
     display.line(x + 3, y + h - 2, x + 3, y + 3)
     
     para(display, s, x + 8, y + 8, w - 16, 1)
+    
+PRG = \
+b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x7f\xff\xff\xfc'\
+b'\x40\x00\x00\x04\x55\x55\x55\x56\x40\x00\x00\x06\x7f\xff\xff\xfe'\
+b'\x40\x00\x00\x06\x40\x00\x00\x06\x40\x00\x00\x06\x40\x00\x00\x06'\
+b'\x40\x00\x00\x06\x40\x00\x00\x06\x40\x00\x00\x06\x40\x00\x00\x06'\
+b'\x40\x00\x00\x06\x40\x00\x00\x06\x40\x00\x00\x06\x40\x00\x00\x06'\
+b'\x40\x00\x00\x06\x40\x00\x00\x06\x40\x00\x00\x06\x40\x00\x00\x06'\
+b'\x40\x00\x00\x06\x40\x00\x00\x06\x40\x00\x00\x06\x40\x00\x00\x06'\
+b'\x7f\xff\xff\xfe\x1f\xff\xff\xfe\x00\x00\x00\x00\x00\x00\x00\x00'
+
+DOC = \
+b'\x1f\xff\xff\x80\x10\x00\x00\x80\x10\x00\x00\xe0\x10\x00\x00\xa0'\
+b'\x10\x00\x00\xb8\x10\x00\x00\xa8\x10\x00\x00\xa8\x10\x00\x00\xa8'\
+b'\x10\x00\x00\xa8\x10\x00\x00\xa8\x10\x00\x00\xa8\x10\x00\x00\xa8'\
+b'\x10\x00\x00\xa8\x10\x00\x00\xa8\x10\x00\x00\xa8\x10\x00\x00\xa8'\
+b'\x10\x00\x00\xa8\x10\x00\x00\xa8\x10\x00\x00\xa8\x10\x00\x00\xa8'\
+b'\x10\x00\x3f\xa8\x10\x00\x21\xa8\x10\x00\x23\x28\x10\x00\x26\x28'\
+b'\x10\x00\x2c\x28\x10\x00\x38\x28\x1f\xff\xf0\x28\x04\x00\x00\x28'\
+b'\x07\xff\xff\xe8\x01\x00\x00\x08\x01\xff\xff\xf8\x00\x00\x00\x00'
+
+icons = {
+    "DOC": DOC,
+    "PRG": PRG
+    }
+
+def icon(display, i, s, x, y):
+    display.pen(0)
+    display.thickness(1)
+    display.font("bitmap6")
+    size = display.measure_text(s, 1.0) / 2
+    off = int(16 - size)
+    display.text(s, x + off, y + 33)
+    display.image(bytearray(icons[i]), 32, 32, x, y)    
+    
+def desktop(display):
+    background(display)
+    menu(display)
+    window(display, 'C:\\BADGER\\', 8, 24, badger2040.WIDTH - 16, badger2040.HEIGHT - 32)
+    icon(display, 'PRG', 'BADGE.PRG', 32, 48)
+    icon(display, 'DOC', 'READ.ME', 128, 48)
+    
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import badger2040
-from desktop import desktop, window, msgbox
+from desktop import desktop
 
 badger2040.system_speed(badger2040.SYSTEM_NORMAL)
 
@@ -7,5 +7,4 @@ display = badger2040.Badger2040()
 display.led(128)
 
 desktop(display)
-window(display, 'Hello', 8, 24, badger2040.WIDTH - 16, badger2040.HEIGHT - 32)
 display.update()
